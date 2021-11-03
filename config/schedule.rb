c964d7a7494d1f66d8f12e400a5943656e267174
Gergo Karcagi <karcagigergo@gmail.com>
2021-11-03 17:50:09 -0300

    made a cron scheduler updating the users every day at 4:30AM

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,14 @@
+require 'pry-byebug'
+require "json"
+require 'rest-client'
+
 env :PATH, ENV['PATH']
 set :output, "log/cron.log"
 
-every 2.minutes do
-  rake 'sample:test'
+# every 2.minutes do
+#   rake 'sample:test'
+# end
+
+every 1.day, at: '4:30 am' do
+  rake 'update:user_database'
 end
