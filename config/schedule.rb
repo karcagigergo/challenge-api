f31ead757720362130419f5a409fd0f63cac8c1f
Gergo Karcagi <karcagigergo@gmail.com>
2021-11-03 18:38:34 -0300

    the scheduled job will happen until User.count <= 2000

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,10 +5,6 @@ require 'rest-client'
 env :PATH, ENV['PATH']
 set :output, "log/cron.log"
 
-# every 2.minutes do
-#   rake 'sample:test'
-# end
-
 every 1.day, at: '4:30 am' do
   rake 'update:user_database'
 end
