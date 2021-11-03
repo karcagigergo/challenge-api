569484a9534acec78b29c13298d27b984f3cc1d0
Gergo Karcagi <karcagigergo@gmail.com>
2021-11-02 22:47:01 -0300

    added admin field to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_023026) do
+ActiveRecord::Schema.define(version: 2021_11_03_014541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_023026) do
     t.json "picture", null: false
     t.json "registered", null: false
     t.string "authentication_token", limit: 30
+    t.boolean "admin", default: false, null: false
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
